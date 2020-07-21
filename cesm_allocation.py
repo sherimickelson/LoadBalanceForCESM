9dc49cff21118068c61e5a7628586cd4d48978c1
Herok4Build <thomash.johnson261@gmail.com>
2020-07-21 14:05:45 -0600

    Bug fixes.

--- cesm_allocation.py
+++ cesm_allocation.py
@@ -59,7 +59,7 @@ def collect_timing_files_one_folder(list_record_of_timing_files):
         collection_of_recent_timing_files.append(temporaryList[0])
         print(collection_of_recent_timing_files)
     for collectedFileItem in collection_of_recent_timing_files:
-        filenameAndPath = collectedFileItem[0]
+        filenameAndPath = collectedFileItem
         filenameExtractionRe = re.search(".*/cesm_timing/",filenameAndPath)
         timing_file_name=filenameAndPath.replace(filenameExtractionRe.group(1),"")
         shutil.copyfile(collectedFileItem, "/glade/work/"+os.environ["USER"]+"/load_balancing_access_timing/"+timing_file_name)
